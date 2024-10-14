60524ce52b200c6e6bb6eb347c301e41776b0fac
aeum1016 <aeum1016@gmail.com>
2024-10-14 16:49:39 -0400

    Add task route

--- controllers/task_controller.go
+++ controllers/task_controller.go
@@ -10,31 +10,43 @@ import (
 type TaskController interface {
 	GetAllTasks() []models.Task
 	GetTaskByID(ctx *gin.Context) models.Task
+	AddTask(ctx *gin.Context) (models.Task, error)
 }
 
-func GetAllTasks() []models.Task {
-	tasks := []models.Task{
+var ExampleTasks = []models.Task{
 	{
-		ID: "123456",
-		UID: "12345566",
-		Title: "My Task",
-		Description: "This is a description for a task 1",
-		DueDate: time.Now().AddDate(0, 1, 0),
-		Priority: 2,
+		ID:            "123456",
+		UID:           "12345566",
+		Title:         "My Task",
+		Description:   "This is a description for a task 1",
+		DueDate:       time.Now().AddDate(0, 1, 0),
+		Priority:      2,
 		EstimateHours: 2,
-		Completed: false,
-	}, 
+		Completed:     false,
+	},
 	{
-		ID: "123457",
-		UID: "12345566",
-		Title: "My Task 2",
-		Description: "This is a description for a task 2",
-		DueDate: time.Now().AddDate(0, 1, 1),
-		Priority: 3,
+		ID:            "123457",
+		UID:           "12345566",
+		Title:         "My Task 2",
+		Description:   "This is a description for a task 2",
+		DueDate:       time.Now().AddDate(0, 1, 1),
+		Priority:      3,
 		EstimateHours: 4,
-		Completed: false,
+		Completed:     false,
 	},
 }
 
-	return tasks
+func GetAllTasks() []models.Task {
+	return ExampleTasks
+}
+
+func AddTask(ctx *gin.Context) (models.Task, error) {
+	var newTask models.Task
+
+	if err := ctx.ShouldBind(&newTask); err != nil {
+		return models.Task{}, err
+	}
+
+	ExampleTasks = append(ExampleTasks, newTask)
+	return newTask, nil
 }
--- routes/task/task_routes.go
+++ routes/task/task_routes.go
@@ -8,9 +8,29 @@ import (
 )
 
 func InitTaskRoutes(r *gin.Engine) {
-	tr := r.Group("/task") 
-	
-	tr.GET("/", func(ctx *gin.Context) {
+	tr := r.Group("/task")
+
+	tr.GET("/", getAllTasks())
+	tr.POST("/", addTask())
+
+}
+
+func getAllTasks() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, controllers.GetAllTasks())
-	})
+	}
+}
+
+func addTask() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		task, err := controllers.AddTask(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"Error": err.Error(),
+			})
+		}
+		if err == nil {
+			ctx.JSON(http.StatusOK, task)
+		}
+	}
 }
