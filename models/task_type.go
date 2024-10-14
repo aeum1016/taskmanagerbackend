da83dff1129d9e50b4acf2cd8e961cb52bbcb299
Alex Eum <aeum1003@gmail.com>
2024-10-13 23:52:29 -0400

    Update Task enum

--- models/task_type.go
+++ models/task_type.go
@@ -6,8 +6,10 @@ type Task struct {
 	ID        string    `json:"id"`
 	UID       string    `json:"userid"`
 	Title     string    `json:"title"`
+	Description string `json:"description"`
 	DueDate   time.Time `json:"duedate"`
 	Priority  uint8     `json:"priority"`
+	EstimateHours uint8 `json:"estimatehours"`
 	Completed bool      `json:"completed"`
 }
 
