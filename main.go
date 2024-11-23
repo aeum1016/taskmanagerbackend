997253c14811f858a8bdb8fce19514959d8695f9
aeum1016 <aeum1016@gmail.com>
2024-11-23 17:50:26 -0500

    Remove tasks that are expired at the midnight mark. Remove expired sessions every 15 minutes using scheduler.

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"github.com/aeum1016/taskmanagerbackend/controllers/session_controller"
+	"github.com/aeum1016/taskmanagerbackend/controllers/task_controller"
 	"github.com/aeum1016/taskmanagerbackend/models"
 	"github.com/aeum1016/taskmanagerbackend/routes"
+	"github.com/aeum1016/taskmanagerbackend/util"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -21,32 +24,23 @@ func main() {
 	connection := models.DBConnection()
 	defer connection.Close()
 
-	ticker := time.NewTicker(30 * time.Minute)
-	quit := make(chan bool)
-	defer func() {
-		ticker.Stop()
-		quit <- true
-	}()
+	util.Schedule(context.Background(), 24*time.Hour, 0, func(t time.Time) {
+		fmt.Println("Removing completed tasks", time.Now().GoString())
+		task_controller.RemoveCompletedTasks()
+	})
 
-	go func(quit chan bool) {
-			for {
-				 select {
-					case <- ticker.C:
-						session_controller.RemoveExpiredSessions()
-					case <- quit:
-						ticker.Stop()
-						return
-					}
-			}
-	 }(quit)
+	util.Schedule(context.Background(), 15*time.Minute, 0, func(t time.Time) {
+		fmt.Println("Removing expired sessions", time.Now().GoString())
+		session_controller.RemoveExpiredSessions()
+	})
 
 	r := gin.Default()
-	
+
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:3000"}
 	config.AllowCredentials = true
 	r.Use(cors.New(config))
-	
+
 	routes.InitRoutes(r)
 	r.Run(":8080")
 }
