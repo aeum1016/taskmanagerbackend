674ed5e0ec3a644e00dc30d1ee5fc217f1a66c3e
aeum1016 <aeum1016@gmail.com>
2024-11-27 22:06:03 -0500

    Add functionality to get user access token

--- models/user_type.go
+++ models/user_type.go
@@ -1,9 +1,5 @@
 package models
 
-import "github.com/google/uuid"
-
-type User struct {
-	ID       uuid.UUID `json:"id" db:"id"`
-	Username string    `json:"username" db:"username" binding:"required"`
-	Password string    `json:"password" db:"password" binding:"required"`
+type UserAuth struct {
+	Auth string `json:"auth" db:"access_token" binding:"required"`
 }
---
+++ routes/user/user_routes.go
@@ -0,0 +1,29 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/aeum1016/taskmanagerbackend/controllers/user_controller"
+	"github.com/aeum1016/taskmanagerbackend/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func InitUserRoutes(r *gin.Engine) {
+	tr := r.Group("/task")
+
+	tr.Use(middleware.AuthMiddleware())
+	{
+		tr.GET("", getUserAuth())
+	}
+}
+
+func getUserAuth() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth, err := user_controller.GetUserAuth(ctx)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.JSON(http.StatusOK, auth)
+	}
+}
